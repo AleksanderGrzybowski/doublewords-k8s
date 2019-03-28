59b44b7b32763982a369a733a8493f7ebf3f8ea6
Aleksander Grzybowski <kelog@orange.pl>
2019-03-28 17:50:45 +0100

    Prototype of wordlist fetching via HTTP

--- src/main/java/pl/kelog/Main.java
+++ src/main/java/pl/kelog/Main.java
@@ -1,7 +1,34 @@
 package pl.kelog;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+        String content = fetchWordsFile("http://storage.kelog.pl/part.txt");
+        System.out.println(content);
+        
+    }
+    
+    private static String fetchWordsFile(String url) throws IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+        
+        HttpResponse response = client.execute(request);
+        
+        BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        
+        String content = "";
+        String line;
+        while ((line = rd.readLine()) != null) {
+            content += line + "\n";
+        }
+        return content;
     }
 }
