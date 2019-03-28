e98aea0e4b4f3cb02ebcbad662daa7d85667fa7e
Aleksander Grzybowski <kelog@orange.pl>
2019-03-28 17:55:37 +0100

    Basic algo for doublewords searching

--- src/main/java/pl/kelog/Main.java
+++ src/main/java/pl/kelog/Main.java
@@ -8,12 +8,29 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String content = fetchWordsFile("http://storage.kelog.pl/part.txt");
         System.out.println(content);
         
+        List<String> words = new ArrayList<>(Arrays.asList(content.split("\n")));
+        System.out.println(words);
+        
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+                if (words.contains(words.get(i) + words.get(j))) {
+                    publish(words.get(i), words.get(j));
+                }
+            }
+        }
+    }
+    
+    private static void publish(String first, String second) {
+        System.out.println("Found: " + first + "-" + second);
     }
     
     private static String fetchWordsFile(String url) throws IOException {
